9bab625db22c901df379a27e4cdc5a388b053568
Mahin Utsman Nawawi <mahinutsmannawawi20@gmail.com>
2026-07-23 08:27:19 +0700

    fix: allow dimzzy.my.id subdomains in preview host configuration

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ import tailwindcss from '@tailwindcss/vite'
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
+  preview: {
+    allowedHosts: ['.dimzzy.my.id'],
+  },
 })
 
 export default config
